e97f468acf4a2dc7534c9941a10876bbf497567c
Vulxn <56614605+Vulxn@users.noreply.github.com>
2021-02-18 18:33:45 -0600

    Add files via upload

---
+++ Portfolio/js/scripts.js
@@ -0,0 +1,126 @@
+$(window).on("load", function() {
+
+
+	$(".loader").fadeOut(1000);
+})
+
+
+$(".items").isotope({
+
+		filter: '*',
+		animationOptions: {
+			duration:1500,
+			easing: 'linear',
+			queue: false
+
+	}
+});
+
+
+$(document).ready(function() {
+
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Web Developer.","Software Engineer.", "Project Manager.","Web Developer."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+
+	});
+
+		$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	})
+		
+	var skillsTopOffset = $(".skillsSection").offset().top;
+
+	$(window).scroll(function() {
+
+		if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+
+			$('.chart').easyPieChart({
+	           easing: 'easeInOut',
+	           barColor: '#000',
+	           trackColor: false,
+	           scaleColor: false,
+	           lineWidth: 4,
+	           size: 152,
+	           onStep: function(from, to, percent) {
+	           	$(this.el).find('.percent').text(Math.round(percent));
+           }
+        });
+		}
+
+
+
+	});
+
+
+	$("[data-fancybox]").fancybox();
+
+
+	$("#filters a").click(function() {
+
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+
+		var selector = $(this).attr("data_filter");
+
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration:1500,
+				easing: 'linear',
+				queue: false
+			}
+
+		});
+
+		return false;
+
+	});
+
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+		var body = $("body");
+
+		if ($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px")
+			body.addClass("fixedNav");		
+
+		}
+
+		else {
+			body.css("padding-top", 0)
+			body.removeClass("fixedNav");
+
+		}		
+	}
+
+});
